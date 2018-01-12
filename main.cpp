a5ec3343c24636bf3e9d76afb3345e0b0c84a526
Michael Baker <michaelpro@MichaeldeMacBook-Pro.local>
2018-01-12 16:07:51 +0800

    LC309

---
+++ main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+        vector<int> buy(n + 1);
+        vector<int> sell(n + 1);
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        sell[-1] = 0;
+        for (int i = 1; i < n; i++){
+            buy[i] = max(buy[i - 1] , sell[i - 2] - prices[i]);
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i]);
+        }
+        return sell[n - 1];
+    }
+};
